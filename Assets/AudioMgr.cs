791d3a0ea4dcb9c119c6745f1e57b2ff1307be3a
laijingfeng <337292616@qq.com>
2017-03-06 20:00:28 +0800

    update

---
+++ Assets/AudioMgr.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Jerry;
+
+public class AudioMgr : SingletonMono<AudioMgr>
+{
+    private AudioSource m_AudioSource;
+    public AudioClip m_Jump; 
+
+    public override void Awake()
+    {
+        base.Awake();
+        m_AudioSource = this.GetComponent<AudioSource>();
+
+        JerryEventMgr.AddEvent(Enum_Event.PlayAudio.ToString(), EventPlayAudio);
+    }
+
+    private void EventPlayAudio(object[] args)
+    {
+        if (args == null || args.Length != 1)
+        {
+            return;
+        }
+        Enum_Audio au = (Enum_Audio)args[0];
+        switch (au)
+        {
+            case Enum_Audio.Jump:
+                {
+                    m_AudioSource.PlayOneShot(m_Jump);
+                }
+                break;
+        }
+    }
+
+    void OnDestroy()
+    {
+        JerryEventMgr.RemoveEvent(Enum_Event.PlayAudio.ToString(), EventPlayAudio);
+    }
+}
+
+public enum Enum_Event
+{
+    None = 0,
+    PlayAudio,
+}
+
+public enum Enum_Audio
+{
+    None = 0,
+    Jump,
+}
