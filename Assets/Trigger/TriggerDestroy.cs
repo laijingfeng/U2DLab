51cd207e3622ae39995db15187a541c53ea5f5de
laijingfeng <337292616@qq.com>
2017-03-07 23:57:42 +0800

    update

---
+++ Assets/Trigger/TriggerDestroy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDestroy : MonoBehaviour
+{
+    public TriggerLightMoon moon;
+
+    private bool finished = false;
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!finished)
+        {
+            finished = true;
+            this.StartCoroutine("Work");
+        }
+    }
+
+    private IEnumerator Work()
+    {
+        if (moon != null)
+        {
+            moon.Work();
+        }
+        yield return new WaitForSeconds(2f);
+        if (this.transform.parent != null)
+        {
+            GameObject.Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
---
+++ Assets/Trigger/TriggerLightMoon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLightMoon : MonoBehaviour
+{
+    private Light light;
+
+    void Awake()
+    {
+        light = this.transform.GetComponent<Light>();
+    }
+
+    public void Work()
+    {
+        this.StartCoroutine("LightMoon");
+    }
+
+    private IEnumerator LightMoon()
+    {
+        float val = 4, per = 0.05f;
+        while (true)
+        {
+            light.intensity += per;
+            val -= per;
+            if (val <= 0)
+            {
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
